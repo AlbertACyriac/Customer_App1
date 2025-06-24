04300a2f9473c05cc2e1765f0e6768d6cb2b05f8
AlbertACyriac <120127617+AlbertACyriac@users.noreply.github.com>
2025-06-24 22:28:50 +0100

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,9 @@
+def display_main_menu():
+    print("\nCustomer Management System")
+    print("1. Display All Customers")
+    print("2. Add Customer")
+    print("3. Amend Customer")
+    print("4. Delete Customer")
+    print("5. View Customer by ID")
+    print("6. Exit")
+    return input("Enter your choice: ")
